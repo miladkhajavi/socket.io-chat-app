8fc5011e9d5a439fbf0c21ecf5d9268deeff2b21
milad khajavi <mkhajavi5@gmail.com>
2020-08-23 13:02:22 +0430

    send message and broadcast events

--- public/js/chat-client.js
+++ public/js/chat-client.js
@@ -1,10 +1,25 @@
 const socket = io()
 
-socket.on('countUpdate', (count)=>{
-    console.log('client on count update' ,count);
+
+// events************************* 
+// socket.on('countUpdate', (count)=>{
+//     console.log('client on count update' ,count);
+// })
+
+// document.querySelector('#increase').addEventListener('click', ()=>{
+//     console.log('added');
+//     socket.emit('increase')
+// })
+// ********************************
+
+socket.on('message', (message)=>{
+    console.log(message);
 })
 
-document.querySelector('#increase').addEventListener('click', ()=>{
-    console.log('added');
-    socket.emit('increase')
+document.querySelector('#message-form').addEventListener('submit' , (e) =>{
+    e.preventDefault()
+
+    const message = e.target.elements.messages.value
+    // console.log(message);
+    socket.emit('sendMSG',message)
 })
--- src/index.js
+++ src/index.js
@@ -16,15 +16,32 @@ let count = 0;
 io.on("connection", (socket) => {
   console.log("new connection");
 
-  socket.emit('countUpdate', count)
+  // events*************************
+  // socket.emit('countUpdate', count)
 
-  socket.on('increase' , ()=>{
-      count++
-    //   socket.emit('countUpdate',count)
-      io.emit('countUpdate',count)
+  // socket.on('increase' , ()=>{
+  //     count++
+  //   //   socket.emit('countUpdate',count)
+  //     io.emit('countUpdate',count)
+  // })
+  // ********************************
+
+  // welcome user ******************
+  socket.emit("message", "welcome");
+  socket.broadcast.emit("message", "new user joined the chat");
+  // *******************************
+  // send message ******************
+  socket.on("sendMSG", (message) => {
+    // console.log(message);
+    io.emit("message", message);
+  });
+  // *******************************
+  // disconnected ******************
+  socket.on('disconnect' , ()=>{
+    io.emit('message' , 'A user has left the chat')
   })
+  // *******************************
 });
-
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
